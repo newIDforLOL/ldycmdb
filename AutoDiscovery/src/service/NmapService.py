553c48f59e51feea0c590871aa27290b49af8413
newIDforLOL <liweieva_09@163.com>
2018-07-19 14:18:01 +0800

    Update NmapService.py

--- AutoDiscovery/src/service/NmapService.py
+++ AutoDiscovery/src/service/NmapService.py
@@ -2,6 +2,7 @@
 
 
 from utils.IniUtils import IniUtils
+import subprocess
 
 
 class NmapService(object):
@@ -9,11 +10,17 @@ class NmapService(object):
     def scan(self):
         nmapCmd = self.getCmd()
         print(nmapCmd)
+        p = subprocess.Popen(nmapCmd, stdout=subprocess.PIPE)
+        parseNmapResult(p.stdout.read())
 
     def getCmd(self):
         ini = IniUtils("nmap.ini")
+        nmapbin = ini.getProperties("nmap", "bin")
         param = ini.getProperties("nmap", "param")
         ifDevice = ini.getProperties("nmap", "ifDevice")
         portRange = ini.getProperties("nmap", "portRange")
         networkSegment = ini.getProperties("nmap", "networkSegment")
-        return "nmap %s -e%s -p%s %s" % (param, ifDevice, portRange, networkSegment)
+        return "%s %s -e %s -p%s %s" % (nmapbin, param, ifDevice, portRange, networkSegment)
+    
+    def parseNmapResult(nmapResult):
+        print(nmapResult)
